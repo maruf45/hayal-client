f06b4776f706a37831d48e8fc7b9a28704aeef1a
maruf45 <armaruf450@gmail.com>
2022-11-25 16:46:35 +0600

    update scroll bar

--- src/Pages/Navbar/Navbar.js
+++ src/Pages/Navbar/Navbar.js
@@ -16,10 +16,10 @@ const Navbar = () => {
   return (
     <>
       <div className="isolate bg-white">
-        <div className="px-6 pt-6 lg:px-8">
+        <div className="px-6 pt-3 lg:px-8">
           <div>
             <nav
-              className="flex h-9 items-center justify-between"
+              className="flex pb-3 items-center justify-between"
               aria-label="Global"
             >
               <div className="flex lg:min-w-0 lg:flex-1" aria-label="Global">
@@ -33,7 +33,6 @@ const Navbar = () => {
                   className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
                   onClick={() => setMobileMenuOpen(true)}
                 >
-                  <span className="sr-only">Open main menu</span>
                   <Bars3Icon className="h-6 w-6" aria-hidden="true" />
                 </button>
               </div>
@@ -95,12 +94,12 @@ const Navbar = () => {
                       ))}
                     </div>
                     <div className="py-6">
-                      <a
-                        href="#"
+                      <Link
+                        to="/login"
                         className="-mx-3 block rounded-lg py-2.5 px-3 text-base font-semibold leading-6 text-gray-900 hover:bg-gray-400/10"
                       >
                         Log in
-                      </a>
+                      </Link>
                     </div>
                   </div>
                 </div>

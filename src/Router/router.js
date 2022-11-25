eef4c33ea9faaf66e786446a547beacc2ee8a7d7
maruf45 <armaruf450@gmail.com>
2022-11-25 18:14:07 +0600

    added login form

--- src/Router/router.js
+++ src/Router/router.js
@@ -1,11 +1,15 @@
 import { createBrowserRouter } from "react-router-dom";
 import Home from "../Pages/Home/Home";
+import Login from "../Pages/Login/Login";
 import MainLayout from "../Pages/MainLayout/MainLayout";
 
 export const router = createBrowserRouter([
   {
     path: "/",
     element: <MainLayout />,
-    children: [{ path: "/", element: <Home /> }],
+    children: [
+      { path: "/", element: <Home /> },
+      { path: "/login", element: <Login /> },
+    ],
   },
 ]);

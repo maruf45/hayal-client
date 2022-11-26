cc049984aad9b399001758aa7381a5f84945edc3
maruf45 <armaruf450@gmail.com>
2022-11-26 10:52:14 +0600

    update home

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -12,13 +12,13 @@ const Home = () => {
                 <h1 className="text-4xl font-bold tracking-tight sm:text-center sm:text-6xl text-white">
                 Find Your Next Match
                 </h1>
-                <p className="mt-6 text-lg leading-8 text-gray-600 sm:text-center text-neutral-100">
+                <p className="mt-6 text-lg leading-8 text-gray-400 sm:text-center text-neutral-100">
                 Find the right price, dealer and advice.
                 </p>
                 <div className="mt-8 flex gap-x-4 sm:justify-center">
                   <Link
                     to="/"
-                    className="inline-block rounded-lg bg-neutral-600 px-4 py-1.5 text-base font-semibold leading-7 text-white shadow-sm ring-1 ring-gray-600 hover:bg-gray-600 hover:ring-gray-600"
+                    className="inline-block rounded-lg bg-gray-600 px-4 py-1.5 text-base font-semibold leading-7 text-white shadow-sm ring-1 ring-gray-600 hover:bg-gray-700 hover:ring-gray-600"
                   >
                     New Cars
                     <span className="text-indigo-200" aria-hidden="true">
@@ -26,7 +26,7 @@ const Home = () => {
                   </Link>
                   <Link
                     to="/"
-                    className="inline-block rounded-lg px-4 py-1.5 text-base font-semibold leading-7 text-neutral-100 ring-1 ring-neutral-100 hover:ring-neutral-500"
+                    className="inline-block rounded-lg px-4 py-1.5 text-base font-semibold leading-7 ring-1 text-white ring-neutral-100 hover:bg-neutral-500 hover:ring-neutral-500"
                   >
                     Used Cars
                     <span className="text-gray-400" aria-hidden="true">

60799e42924bb80f5a0016c89797c1cbae1a1355
maruf45 <armaruf450@gmail.com>
2022-11-26 21:57:49 +0600

    update cars category

--- src/Pages/Cars/CarsBrands.js
+++ src/Pages/Cars/CarsBrands.js
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
+import { NavLink } from "react-router-dom";
+import './CarBrands.modules.css'
 
 const CarsBrands = () => {
   const [carsBrands, setCarBrands] = useState([]);
@@ -10,8 +12,23 @@ const CarsBrands = () => {
     };
     fetchCars();
   }, []);
-  console.log(carsBrands);
-  return <>{carsBrands.length}</>;
+  return <>
+  <h1 className="text-5xl text-neutral-500 mt-[120px] mb-[50px] font-bold text-center ">Our Cars Categories</h1>
+  <div className="carsBrands-container flex justify-center mx-auto w-fit rounded-full">
+    <ul className="flex justify-center rounded-full">
+        {carsBrands.map(cars => {
+            return <React.Fragment key={cars._id}>
+            <li className="rounded-full">
+                <NavLink to={`/cars/${cars._id}`}>
+                    {cars.BrandName}
+                </NavLink>
+            </li>
+            </React.Fragment>
+        })}
+    </ul>
+  </div>
+   
+  </>;
 };
 
 export default CarsBrands;
